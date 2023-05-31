f99616f83f23b0a087dcfb92ecc33c52782e0aa3
managne <agne.manaikaite@gmail.com>
2023-05-31 22:36:17 +0300

    feat: 130. Filtering Projects with Enums

--- src/app.ts
+++ src/app.ts
@@ -134,7 +134,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(project => {
+        if ( this.type === 'active'){
+          return project.status === ProjectStatus.Active;
+        } 
+        return project.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -146,6 +152,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     )! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const projectItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = projectItem.title;
